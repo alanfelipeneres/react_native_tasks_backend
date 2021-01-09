cc16c31d4d0d484b09c8fbb004c313dde3e15c87
unknown <alanfelipe.neres@outlook.com>
2021-01-09 16:22:08 -0300

    first commit

---
+++ config/passport.js
@@ -0,0 +1,33 @@
+const { authSecret } = require('../.env')
+const passport = require('passport')
+const passportJwt = require('passport-jwt')
+const { Strategy, ExtractJwt } = passportJwt
+
+module.exports = app => {
+    const params = {
+        secretOrKey: authSecret,
+        jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken()
+    }
+
+    const strategy = new Strategy(params, (payload, done) => {
+        app.db('users')
+            .where({ id: payload.id })
+            .first()
+            .then(user => {
+                if (user) {
+                    //1º parâmetro do done é o erro, por isso recebeu o null
+                    done(null, { id: user.id, email: user.email })
+                } else {
+                    done(null, false)
+                }
+            })
+            .catch(err => done(err, false))
+    })
+
+    passport.use(strategy)
+
+    return {
+        initialize: () => passport.initialize(),
+        authenticate: () => passport.authenticate('jwt', { session: false })
+    }
+}
